a086aca8e8ceb85c73976177dcf35e5db1eccf56
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-08-31 23:02:40 +0000

    comitting this code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,42 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
-});
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'B', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'O', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BOO')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word isn't present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'B', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'O', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LIGHTHOUSE LABS IS GREAT')
+
+    assert.isFalse(result);
+  });
+
+  it("should return undefined if lettersMatrix is empty", function() {
+    const result = wordSearch([], 'LIGHTHOUSE LABS IS GREAT')
+
+    assert.isUndefined(result);
+  });
+})
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    // create container array to house our new matrix
+    const ourMatrix = [];
+    // create new rows
+    for (let i = 0; i < matrix[0].length; i++) { 
+      let subArray = []; // create new subarray
+      ourMatrix.push(subArray); // push new subarray
     }
-}
-
-module.exports = wordSearch
+    for (let i = 0; i < matrix.length; i++) { // i = rows of the matrix, columns of ourMatrix
+      for (let j = 0; j < matrix[0].length; j++) { // j = columns of the matrix, rows of ourMatrix
+        ourMatrix[j].push(matrix[i][j]);
+      }
+    }
+    return ourMatrix;
+  };
+  
+  const wordSearch = (lettersMatrix, word) => { 
+    if (!lettersMatrix.length) { return undefined; }
+  
+    const horizontalJoin = lettersMatrix.map(row => row.join(''));
+    for (row of horizontalJoin) {
+      if (row.includes(word)) {
+        return true;
+      }
+    }
+    const newArray = transpose(lettersMatrix);
+    const verticalJoin = newArray.map(row => row.join(''));
+    for (row of verticalJoin) {
+      if (row.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  module.exports = wordSearch
